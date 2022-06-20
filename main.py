16692361e1c495dcca3e14f707d480e4f76c779d
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-06-20 16:54:56 +0200

    [Test-in-production] Fixed a print statement

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ class deauthy:
         try:
             method = deauthy.prompt(f"{light_white}Which {bold}wireless{end}{light_white} interface should be put into monitor mode? Enter corresponding number {light_blue}({yellow}1{white}-{yellow}{ifaces}{light_blue})", yellow)
         except KeyboardInterrupt:
-            deauthy.inform(f"{light_green}{bold} Goodbye!\nExiting...")
+            deauthy.inform(f"\n{light_green}{bold} Goodbye!\nExiting...")
             exit(0)
     class Appearance:
 
