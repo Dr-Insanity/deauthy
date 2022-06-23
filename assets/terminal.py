c960aa7fe2de13bc4b453cbaee238c8ad94d7e3b
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-06-24 00:43:50 +0200

    s

--- assets/terminal.py
+++ assets/terminal.py
@@ -44,18 +44,18 @@ class Terminal:
     DeAuThY = Fore.WHITE + "[" + Fore.RED + "D" + Fore.LIGHTYELLOW_EX + "E" + Fore.LIGHTGREEN_EX + "A" + Fore.MAGENTA + "U" + Fore.CYAN + "T" + Fore.BLUE + "H" + Fore.RED + "Y" + Fore.WHITE + "]"
 
     def tell_issue(self, msg: str):
-        d_wut = self.white + f"{self.bold}[" + self.red + "!" + self.white + f"]{self.end}{self.light_white} "
+        d_wut = self.White + f"{self.Bold}[" + self.Red + "!" + self.White + f"]{self.End}{self.Light_white} "
         print(self.deAuThY + d_wut + msg)
 
     def inform(self, msg: str, entire_color=Fore.LIGHTBLACK_EX):
-        d_hey = self.white + f"{self.bold}[" + self.light_green + "+" + self.white + f"]{self.end}{self.light_white} "
+        d_hey = self.White + f"{self.Bold}[" + self.Light_green + "+" + self.White + f"]{self.End}{self.Light_white} "
         print(self.deAuThY + d_hey + entire_color + msg)
 
     def prompt(self, question: str, allowed_replies: list[str], ending_color=Fore.WHITE) -> str:
-        d_huh = self.white + f"{self.bold}[" + self.light_blue + "?" + self.white + f"]{self.end}{self.light_white} "
-        reply = input(self.deAuThY + d_huh + f"{self.light_white}{question}{self.bold}>{self.end} {ending_color}")
+        d_huh = self.White + f"{self.Bold}[" + self.Light_blue + "?" + self.white + f"]{self.End}{self.Light_white} "
+        reply = input(self.deAuThY + d_huh + f"{self.Light_white}{question}{self.Bold}>{self.End} {ending_color}")
         if reply.lower() in CommandHandler.supported_commands_debian_based_distros:
-            print(self.end)
+            print(self.End)
             check_call(reply)
             reply = self.prompt(question, allowed_replies, ending_color)
             return reply
@@ -69,6 +69,6 @@ class Terminal:
             if allowed_replies[0].lower() == "any":
                 return reply
             else:
-                self.tell_issue(f"{self.red}That's not a valid {self.bold}{self.red}reply{self.end}{self.red} :/")
+                self.tell_issue(f"{self.Red}That's not a valid {self.Bold}{self.Red}reply{self.End}{self.Red} :/")
                 reply = self.prompt(question=question, allowed_replies=allowed_replies)
                 return reply
