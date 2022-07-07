583878525210dc00d24009f5f34ed73d00bea9f8
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-07-07 11:18:12 +0200

    s

--- t.py
+++ t.py
@@ -1,10 +1,12 @@
 import csv
 
 def csv2blob(filename):
-
+    li = []
     with open(filename,'r') as f:
-        z = f.readlines()
+        for line in f.readlines():
+            n = line.strip('\r\n\r\n')
+            li.append(n)
 
-    print(z)
+    print(li)
 
 ai = csv2blob('/home/netmin/Desktop/discovered_targets-01.csv')
