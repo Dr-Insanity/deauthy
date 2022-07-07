763064ad0d3cdf43ecf3bd6b36df5b5430272814
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-07-07 11:11:35 +0200

    s

--- t.py
+++ t.py
@@ -6,8 +6,10 @@ def csv2blob(filename):
         z = f.read()
 
     parts = z.split('\r\n\r\n')
-    print(parts)
+    p_parts = [part for part in parts]
+    print(p_parts)
     stations = parts[0]
-    print(stations)
+    p_stats = [stat for stat in stations]
+    print(p_stats)
 
 ai = csv2blob('/home/netmin/Desktop/discovered_targets-01.csv')
