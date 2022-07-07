8390d648e3a79f62e5cec8664c9349c03b33d8d7
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-07-07 10:53:43 +0200

    s

--- t.py
+++ t.py
@@ -1,5 +1,37 @@
-import pandas
+import csv
 
-df = pandas.read_csv('/home/netmin/Desktop/discovered_targets-01.csv', sep='\t')
+def csv2blob(filename):
 
-print(df.to_dict())
+    with open(filename,'rb') as f:
+        z = f.read()
+
+    parts = z.split('\r\n\r\n')
+    
+    stations = parts[0]
+    
+    clients = parts[1]
+
+    import sys
+    if sys.version_info[0] < 3:
+        from StringIO import StringIO
+    else:
+        from io import StringIO
+
+    stations_str = StringIO(stations)
+    clients_str  = StringIO(clients)
+
+    r = csv.reader(stations_str)
+    i = list(r)
+    z = [k for k in i if k != []]
+
+    stations_list = z
+
+    r = csv.reader(clients_str)
+    i = list(r)
+    z = [k for k in i if k != []]
+
+    clients_list = z
+    
+    return stations_list, clients_list
+
+csv2blob('/home/netmin/Desktop/discovered_targets-01.csv')
