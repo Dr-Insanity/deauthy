8ca53ff19b84fab3bc0d353a57586def4c175a18
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-07-07 11:25:06 +0200

    s

--- t.py
+++ t.py
@@ -10,4 +10,7 @@ def csv2blob(filename: str, pos: int):
 
     print(li[int(pos)])
 
+    print('=======================[TARGETS]=======================')
+    print(li[2::])
+
 ai = csv2blob('/home/netmin/Desktop/discovered_targets-01.csv', sys.argv[1])
