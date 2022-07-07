ba0ce2a125213d8c0a79d6e2670ccf2742b1354e
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-07-07 11:30:00 +0200

    s

--- t.py
+++ t.py
@@ -6,15 +6,12 @@ def csv2blob(filename: str):
     with open(filename,'r') as f:
         for line in f.readlines():
             n = line.strip('\r\n\r\n\t')
+            n = n.strip(f" ")
             li.append(n)
 
-    lo = []
-    for line in li:
-        rr = f"""'{line}'"""
-        lo.append(rr)
 
     print('=======================[TARGETS]=======================')
-    print(lo[2::])
+    print(li[2::])
     print('=======================[TARGETS]=======================')
 
 ai = csv2blob('/home/netmin/Desktop/discovered_targets-01.csv')
