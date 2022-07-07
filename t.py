ae3730510ef413a0a1e0c38599b90549d973c728
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-07-07 11:09:23 +0200

    s

--- t.py
+++ t.py
@@ -10,21 +10,4 @@ def csv2blob(filename):
     stations = parts[0]
     print(stations)
 
-    import sys
-    if sys.version_info[0] < 3:
-        from StringIO import StringIO
-    else:
-        from io import StringIO
-
-    stations_str = StringIO(stations)
-
-    r = csv.reader(stations_str)
-    i = list(r)
-    z = [k for k in i if k != []]
-
-    stations_list = z
-    
-    return stations_list
-
 ai = csv2blob('/home/netmin/Desktop/discovered_targets-01.csv')
-print(ai)
