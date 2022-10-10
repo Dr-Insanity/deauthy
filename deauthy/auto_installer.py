fac6e555702015f231df663d32803f00efd8da76
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-10-10 13:14:27 +0200

    s

--- deauthy/auto_installer.py
+++ deauthy/auto_installer.py
@@ -137,7 +137,7 @@ class DeAuthy():
                 shutil.rmtree(file)
                 shutil.move(f"deauthy-Testing/{file}", "./")
             if os.path.isfile(f"deauthy-Testing/{file}"):
-                spinner.stop_and_persist()
+                #spinner.stop_and_persist()
                 print(file)
                 print(answ.lower())
                 if file in ["conf.json"] and answ.lower() == "y":
@@ -158,7 +158,7 @@ class DeAuthy():
             Terminal.inform(f"{Terminal.Red}Config file is reset.")
         else:
             Terminal.inform(f"{Terminal.Light_green}Config file is kept as-is.")
-        spinner.succeed(f"{Terminal.Light_green} Success! Restarting...")
+        #spinner.succeed(f"{Terminal.Light_green} Success! Restarting...")
         time.sleep(5) # give the time to the user to read that we're restarting
         os.execv(sys.executable, ['python'] + [sys.argv[0]])
 
