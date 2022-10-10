53f1c9e41e55403703906b32366da2cdff13def5
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-10-10 13:03:36 +0200

    s

--- deauthy/auto_installer.py
+++ deauthy/auto_installer.py
@@ -137,7 +137,7 @@ class DeAuthy():
                     shutil.rmtree(file)
                     shutil.move(f"deauthy-Testing/{file}", "./")
                 if os.path.isfile(f"deauthy-Testing/{file}"):
-                    if file in ["conf.json"] and str(keep_config) == "True":
+                    if file in ["conf.json"] and answ.lower() == "y":
                         spinner.stop_and_persist()
                         print("We need to keep config!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                         continue
