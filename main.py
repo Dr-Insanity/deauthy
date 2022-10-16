a33d6763c50556a253b5242c0fa9b52274362b3b
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-10-16 09:53:30 +0200

    s

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ try:
     if not "--no-banner" in argv:
         with Banner():
             time.sleep(5)
-    time.sleep(2)
+    time.sleep(1)
     print(light_green + "Time to kick off some assholes from yer net" + f"\n{white}DeAuthy version: {light_white}{version()}")
     main()
 except KeyboardInterrupt:
