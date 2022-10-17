0af1180145d6bfd6112dffc6dcf2d06a02c5f1c9
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-10-17 20:09:23 +0200

    s

--- deauthy/terminal.py
+++ deauthy/terminal.py
@@ -57,8 +57,7 @@ class Terminal:
             d_huh = f"{Terminal.White}{Terminal.Bold}[{Terminal.Light_white}#{Terminal.White}]{Terminal.End}{Terminal.Light_white} "
         try:
             reply = input(Terminal.deAuThY + d_huh + f"{Terminal.Light_white}{question}{Terminal.Bold}>{Terminal.End} {ending_color}")
-            print(Terminal.End+question)
-            if f"access points to blacklist for" in Terminal.End+question:
+            if f"{Terminal.Cyan+Terminal.Bold+Terminal.Underline}Select access points to blacklist for {Terminal.Red}1 {Terminal.Cyan}client" in question:
                 return reply
             if reply.split()[0].lower() in CommandHandler.Debian.supported_commands:
                 try:
